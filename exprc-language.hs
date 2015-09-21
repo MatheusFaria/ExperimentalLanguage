9a40933f0149133b029e30721ab5a9c5b33a5428
Matheus Faria <matheus.sousa.faria@gmail.com>
2015-09-21 18:26:26 -0300

    Sum binop

--- exprc-language.hs
+++ exprc-language.hs
@@ -1,5 +1,30 @@
+import Data.Maybe
+
 data ExprC =   NumC Float
              | TrueC
              | FalseC
-             | BinOpC ExprC ExprC Char
+             | BinOpC Char ExprC ExprC
              deriving (Show)
+
+data ExprV =   NumV Float
+             | BoolV Bool
+             deriving (Show)
+
+
+sumC :: ExprV -> ExprV -> ExprV
+sumC (NumV a) (NumV b) = (NumV (a + b))
+
+binops = [('+', sumC)]
+
+getBinop :: Char -> (ExprV -> ExprV -> ExprV)
+getBinop op
+        | isNothing func = error "No symbol found"
+        | isJust func = fromJust(func)
+        where func = lookup op binops
+
+interp :: ExprC -> ExprV
+interp (NumC n) = (NumV n)
+interp (TrueC) = (BoolV True)
+interp (FalseC) = (BoolV False)
+interp (BinOpC op a b) = ((getBinop op) (interp a) (interp b))
+
